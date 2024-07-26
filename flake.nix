be0925957ea5d6e07a47ab9ae38a964366451c1a
alxdb <alxdb@pm.me>
2024-07-26 21:37:00 +0100

    initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default =
+          with pkgs;
+          mkShell {
+            buildInputs = [
+              cabal-install
+              ghc
+              hpack
+              # https://github.com/haskell/haskell-language-server/issues/176
+              haskellPackages.hspec-discover
+            ];
+          };
+      }
+    );
+}
---
+++ test/AocSpec.hs
@@ -0,0 +1,6 @@
+module AocSpec where
+
+import Test.Hspec
+
+spec :: Spec
+spec = it "passes tests" pending
