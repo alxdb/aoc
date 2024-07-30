15fb2bf2a46226761121c23127188834e09ff0cd
alxdb <alxdb@pm.me>
2024-07-30 18:55:09 +0100

    init year 2015 day 1 part 2

---
+++ lib/Aoc/Year2015/Day01/Part2.hs
@@ -0,0 +1,6 @@
+module Aoc.Year2015.Day01.Part2 (
+  solution,
+) where
+
+solution :: String -> Either String Int
+solution = undefined
---
+++ test/Aoc/Year2015/Day01/Part2Spec.hs
@@ -0,0 +1,9 @@
+module Aoc.Year2015.Day01.Part2Spec (
+  spec,
+) where
+
+import Aoc.Year2015.Day01.Part2
+import Test.Hspec
+
+spec :: Spec
+spec = describe "solution" undefined
