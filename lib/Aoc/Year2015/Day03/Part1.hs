6d8b59c95c9a469eaabc58fcee81e52cec1e5712
alxdb <alxdb@pm.me>
2024-08-01 22:24:22 +0100

    solve 2015 day03 part1

--- lib/Aoc/Year2015/Day03/Part1.hs
+++ lib/Aoc/Year2015/Day03/Part1.hs
@@ -2,5 +2,29 @@ module Aoc.Year2015.Day03.Part1 (
   solution,
 ) where
 
+import Data.Bifunctor
+import Data.Functor
+import Data.Maybe
+import Data.Set.Ordered (OSet, (|<))
+import Data.Set.Ordered qualified as OSet
+
+data Direction = N | E | S | W deriving (Eq)
+
+readDir :: Char -> Either String Direction
+readDir '^' = Right N
+readDir '>' = Right E
+readDir 'v' = Right S
+readDir '<' = Right W
+readDir c = Left $ "unexpected char: " $> c
+
+lastVisited :: OSet (Int, Int) -> (Int, Int)
+lastVisited = fromJust . flip OSet.elemAt 0
+
 solution :: String -> Either String Int
-solution = const $ Left "unimplemented"
+solution = fmap (OSet.size . foldl f (OSet.singleton (0, 0))) . mapM readDir
+ where
+  f :: OSet (Int, Int) -> Direction -> OSet (Int, Int)
+  f visited N = second succ (lastVisited visited) |< visited
+  f visited E = first succ (lastVisited visited) |< visited
+  f visited S = second pred (lastVisited visited) |< visited
+  f visited W = first pred (lastVisited visited) |< visited
