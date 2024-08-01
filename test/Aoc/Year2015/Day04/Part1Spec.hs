c1a39abb2cff56c03c07fdbb26e29bc689471bdb
alxdb <alxdb@pm.me>
2024-08-01 23:56:00 +0100

    add test cases

--- test/Aoc/Year2015/Day04/Part1Spec.hs
+++ test/Aoc/Year2015/Day04/Part1Spec.hs
@@ -7,4 +7,10 @@ import Aoc.Year2015.Day04.Part1
 import Test.Hspec
 
 spec :: Spec
-spec = describe "solution" $ testCasesSpec solution []
+spec =
+  describe "solution" $
+    testCasesSpec
+      solution
+      [ makeCase "abcdef" 609043
+      , makeCase "pqrstuv" 1048970
+      ]
