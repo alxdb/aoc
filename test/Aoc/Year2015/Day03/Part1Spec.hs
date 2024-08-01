0a1ed90553ce8d3d8e6ceb529c15a0b1a87271b0
alxdb <alxdb@pm.me>
2024-08-01 09:21:11 +0100

    test cases

--- test/Aoc/Year2015/Day03/Part1Spec.hs
+++ test/Aoc/Year2015/Day03/Part1Spec.hs
@@ -7,4 +7,11 @@ import Aoc.Year2015.Day03.Part1
 import Test.Hspec
 
 spec :: Spec
-spec = describe "solution" $ testCasesSpec solution []
+spec =
+  describe "solution" $
+    testCasesSpec
+      solution
+      [ makeCase ">" 2
+      , makeCase "^>v<" 4
+      , makeCase "^v^v^v^v^v" 2
+      ]
