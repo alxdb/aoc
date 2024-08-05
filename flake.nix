a1b55ce6bd73f9ed7cb6fd5b06791a9997f91867
alxdb <alxdb@pm.me>
2024-08-05 22:44:46 +0100

    flake setup

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    zig.url = "github:mitchellh/zig-overlay";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      utils,
+      zig,
+    }:
+    utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        zigPkgs = zig.packages.${system};
+      in
+      {
+        devShell = pkgs.mkShell { buildInputs = with pkgs; [ zigPkgs.default ]; };
+      }
+    );
+}
