b65e599a4395048988e1b8309ac4aed142523615
alxdb <alxdb@pm.me>
2024-08-07 00:59:03 +0100

    2015_01 solution

--- solutions/2015_01.zig
+++ solutions/2015_01.zig
@@ -1,5 +1,47 @@
 const std = @import("std");
 
-pub fn main() void {
-    std.debug.print("I'm a solver!\n", .{});
+const input = @embedFile("input");
+
+fn part1() !i32 {
+    var floor: i32 = 0;
+    for (input) |c| {
+        switch (c) {
+            '(' => {
+                floor += 1;
+            },
+            ')' => {
+                floor -= 1;
+            },
+            else => {
+                return error.InvalidInput;
+            },
+        }
+    }
+    return floor;
+}
+
+fn part2() !usize {
+    var floor: i32 = 0;
+    for (input, 1..) |c, i| {
+        switch (c) {
+            '(' => {
+                floor += 1;
+            },
+            ')' => {
+                floor -= 1;
+            },
+            else => {
+                return error.InvalidInput;
+            },
+        }
+        if (floor < 0) {
+            return i;
+        }
+    }
+    return error.NeverReachedBasement;
+}
+
+pub fn main() !void {
+    std.log.info("answer to part 1 is {}", .{try part1()});
+    std.log.info("answer to part 2 is {}", .{try part2()});
 }
