a540b71d29516668503a0a5bda9204d19eb1c683
alxdb <alxdb@pm.me>
2024-08-18 09:28:37 +0100

    collect input
    Squashed commit of the following:
    
    commit 6ed4bd5bc3656a9c8de96d0ec6a7c1de2fbaa489
    Author: alxdb <alxdb@pm.me>
    Date:   Sun Aug 18 09:25:51 2024 +0100
    
        handle http errors and don't write file on failure
    
    commit 4b56f5654847563ef71d6ecd2a9af19d9a2e9675
    Author: alxdb <alxdb@pm.me>
    Date:   Wed Aug 14 19:02:56 2024 +0100
    
        initial implementation

--- src/bin/aoc.rs
+++ src/bin/aoc.rs
@@ -32,12 +32,14 @@ fn main() -> anyhow::Result<()> {
         Commands::RunSolution { solution } => {
             let aoc_id = parse_solution_name(&solution)?;
             println!("Running solution: {aoc_id:?}");
+            let input = input_cache::get_input(&aoc_id, &cli.aoc_token)?;
+            println!("Fetched input: len={}", input.len());
         }
     }
     Ok(())
 }
 
-pub fn parse_solution_name(solution_name: &str) -> anyhow::Result<AocId> {
+fn parse_solution_name(solution_name: &str) -> anyhow::Result<AocId> {
     static RE: OnceLock<Regex> = OnceLock::new();
     RE.get_or_init(|| Regex::new(r"aoc(\d{2})_(\d{2})$").unwrap())
         .captures(solution_name)
@@ -51,41 +53,45 @@ pub fn parse_solution_name(solution_name: &str) -> anyhow::Result<AocId> {
 
 mod input_cache {
     use crate::AocId;
-    use anyhow::anyhow;
-    use std::fs::OpenOptions;
+    use std::fs::{self, OpenOptions};
     use std::io::{ErrorKind, Write};
-    use std::path::{Path, PathBuf};
 
-    fn input_path(aoc_id: &AocId) -> PathBuf {
-        dirs::cache_dir()
+    fn fetch_input(aoc_id: &AocId, aoc_token: &str) -> anyhow::Result<Vec<u8>> {
+        let response = reqwest::blocking::Client::new()
+            .get(format!(
+                "https://adventofcode.com/20{}/day/{}/input",
+                aoc_id.year, aoc_id.day
+            ))
+            .header("COOKIE", format!("session={aoc_token}"))
+            .send()?
+            .error_for_status()?;
+        Ok(response.bytes()?.into())
+    }
+
+    pub fn get_input(aoc_id: &AocId, aoc_token: &str) -> anyhow::Result<String> {
+        let input_path = dirs::cache_dir()
             .unwrap()
             .join("aoc")
-            .join(format!("input_{}_{}", aoc_id.year, aoc_id.day))
-    }
+            .join(format!("input_{}_{}", aoc_id.year, aoc_id.day));
+        fs::create_dir_all(input_path.parent().unwrap()).or_else(|e| match e.kind() {
+            ErrorKind::AlreadyExists => Ok(()),
+            _ => Err(e),
+        })?;
+        if !input_path.exists() {
+            let input = fetch_input(aoc_id, aoc_token)?;
+            match OpenOptions::new()
+                .write(true)
+                .create_new(true)
+                .open(&input_path)
+            {
+                Ok(mut f) => f.write_all(&input)?,
+                Err(e) => match e.kind() {
+                    ErrorKind::AlreadyExists => (),
+                    _ => return Err(anyhow::Error::from(e)),
+                },
+            }
+        }
 
-    fn fetch_input(aoc_id: &AocId, aoc_token: &str) -> Vec<u8> {
-        todo!()
-    }
-
-    fn initialize_input(aoc_id: &AocId, aoc_token: &str) -> anyhow::Result<()> {
-        OpenOptions::new()
-            .write(true)
-            .create_new(true)
-            .open(input_path(aoc_id))
-            .and_then(|mut f| f.write_all(&fetch_input(aoc_id, aoc_token)))
-            .or_else(|e| match e.kind() {
-                ErrorKind::AlreadyExists => Ok(()),
-                _ => Err(e),
-            })
-            .map_err(anyhow::Error::from)
-    }
-
-    fn read_input(aoc_id: &AocId) -> anyhow::Result<String> {
-        todo!()
-    }
-
-    fn get_input(aoc_id: &AocId, aoc_token: &str) -> anyhow::Result<String> {
-        initialize_input(aoc_id, aoc_token)?;
-        todo!()
+        Ok(fs::read_to_string(&input_path)?)
     }
 }
