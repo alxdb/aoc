3d0d70a2a19462b22da1e49aad28d80f4efcb912
alxdb <alxdb@pm.me>
2024-08-18 18:33:22 +0100

    solve 2

--- src/bin/aoc15_02.rs
+++ src/bin/aoc15_02.rs
@@ -1,9 +1,32 @@
 include!(concat!(env!("OUT_DIR"), "/aoc_prefix.rs"));
 
-fn part1(_input: &str) -> u64 {
-    0
+fn parse_line(line: &str) -> [i64; 3] {
+    line.split('x')
+        .map(|s| s.parse().unwrap())
+        .collect::<Vec<_>>()
+        .try_into()
+        .unwrap()
 }
 
-fn part2(_input: &str) -> u64 {
-    0
+fn part1(input: &str) -> i64 {
+    input
+        .lines()
+        .map(parse_line)
+        .map(|x| {
+            let side_areas = [x[0] * x[1], x[1] * x[2], x[0] * x[2]];
+            side_areas.into_iter().map(|x| x * 2).sum::<i64>()
+                + side_areas.into_iter().min().unwrap()
+        })
+        .sum()
+}
+
+fn part2(input: &str) -> i64 {
+    input
+        .lines()
+        .map(parse_line)
+        .map(|mut x| {
+            x.sort();
+            x[0] * 2 + x[1] * 2 + x.into_iter().product::<i64>()
+        })
+        .sum()
 }
