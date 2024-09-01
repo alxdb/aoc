00f46ee5448188009d1037d4c95abb58194498b8
alxdb <alxdb@pm.me>
2024-09-01 15:19:00 +0100

    extract day4 solution to common function

--- src/bin/aoc15_04.rs
+++ src/bin/aoc15_04.rs
@@ -1,25 +1,21 @@
 include!(concat!(env!("OUT_DIR"), "/aoc_prefix.rs"));
 
-fn part1(input: &str) -> u64 {
+fn mine(input: &str, prefix: &str) -> u64 {
     let mut salt = 0;
     loop {
         let input = format!("{}{}", input.strip_suffix("\n").unwrap(), salt);
         let digest = md5::compute(input);
-        if format!("{:x}", digest).starts_with("00000") {
+        if format!("{:x}", digest).starts_with(prefix) {
             return salt;
         }
         salt += 1;
     }
 }
 
+fn part1(input: &str) -> u64 {
+    mine(input, "00000")
+}
+
 fn part2(input: &str) -> u64 {
-    let mut salt = 0;
-    loop {
-        let input = format!("{}{}", input.strip_suffix("\n").unwrap(), salt);
-        let digest = md5::compute(input);
-        if format!("{:x}", digest).starts_with("000000") {
-            return salt;
-        }
-        salt += 1;
-    }
+    mine(input, "000000")
 }
