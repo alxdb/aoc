414f4d10887477ccdc8e28f27490e9851c81bc8b
alxdb <alxdb@pm.me>
2024-10-09 08:22:34 +0100

    Solve problem

--- lib/Aoc/Year2015/Day04/Part1.hs
+++ lib/Aoc/Year2015/Day04/Part1.hs
@@ -1,6 +1,22 @@
 module Aoc.Year2015.Day04.Part1 (
-  solution,
+    solution,
 ) where
 
+import Control.Arrow
+import Crypto.Hash.MD5 qualified as MD5
+import Data.ByteString.Builder qualified as BSB
+import Data.ByteString.Char8 qualified as BSC
+import Data.ByteString.Lazy qualified as BSL
+import Data.ByteString.Lazy.UTF8 qualified as BSLU
+
 solution :: String -> Either String Int
-solution = const $ Left "unimplemented"
+solution s = Right $ head $ filter (\i -> hashPrefix (s <> show i) == "00000") [0 ..]
+
+hashPrefix :: String -> String
+hashPrefix =
+    BSC.pack
+        >>> MD5.hash
+        >>> BSB.byteStringHex
+        >>> BSB.toLazyByteString
+        >>> BSL.take 5
+        >>> BSLU.toString
