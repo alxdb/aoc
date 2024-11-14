c9b1cd838dfe88096b10b5b3e54b670dee0813b0
alxdb <alxdb@pm.me>
2024-11-14 00:46:47 +0000

    Parallel solution for year 2015 day 14

--- src/bin/aoc15_04.rs
+++ src/bin/aoc15_04.rs
@@ -1,3 +1,11 @@
+use std::{
+    sync::{
+        atomic::{AtomicBool, AtomicU64, Ordering},
+        Arc,
+    },
+    thread::available_parallelism,
+};
+
 aoc::main!();
 
 fn test_salt(mut input: String, salt: u64, mut leading_zeros: usize) -> bool {
@@ -20,14 +28,34 @@ fn test_salt(mut input: String, salt: u64, mut leading_zeros: usize) -> bool {
 }
 
 fn mine(input: &str, leading_zeros: usize) -> u64 {
+    // This may return a value that is not the smallest possible salt
+    // May be aleviated with barriers and `fetch_min`, but barriers lead to hanging
     let input = input.strip_suffix("\n").unwrap().to_owned();
-    let mut salt = 0;
-    loop {
-        if test_salt(input.clone(), salt, leading_zeros) {
-            return salt;
+    std::thread::scope(|s| {
+        let stop = Arc::new(AtomicBool::new(false));
+        let result = Arc::new(AtomicU64::new(0));
+        let n_workers = available_parallelism().unwrap().get();
+        let step = n_workers as u64;
+        for i in 0..step {
+            let input = input.clone();
+            let stop = stop.clone();
+            let result = result.clone();
+            s.spawn(move || {
+                let mut salt = i;
+                while !stop.load(Ordering::Relaxed) {
+                    if test_salt(input.clone(), salt, leading_zeros) {
+                        result.store(salt, Ordering::Relaxed);
+                        stop.store(true, Ordering::Relaxed);
+                    }
+                    salt += step;
+                }
+            });
         }
-        salt += 1;
-    }
+        while !stop.load(Ordering::Relaxed) {
+            std::hint::spin_loop();
+        }
+        result.load(Ordering::Relaxed)
+    })
 }
 
 fn part1(input: &str) -> u64 {
