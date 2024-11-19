255bb5f0f0d33381cb5e1c261dc6f61cd9b103c5
alxdb <alxdb@pm.me>
2024-11-19 07:50:40 +0000

    aoc15_04: Correct parallel solution

--- src/bin/aoc15_04.rs
+++ src/bin/aoc15_04.rs
@@ -1,6 +1,6 @@
 use std::{
     sync::{
-        atomic::{AtomicBool, AtomicU64, Ordering},
+        atomic::{AtomicU64, Ordering},
         Arc,
     },
     thread::available_parallelism,
@@ -28,31 +28,30 @@ fn test_salt(mut input: String, salt: u64, mut leading_zeros: usize) -> bool {
 }
 
 fn mine(input: &str, leading_zeros: usize) -> u64 {
-    // This may return a value that is not the smallest possible salt
-    // May be aleviated with barriers and `fetch_min`, but barriers lead to hanging
     let input = input.strip_suffix("\n").unwrap().to_owned();
+    let result = Arc::new(AtomicU64::new(u64::MAX));
+    let n_workers = available_parallelism().unwrap().get();
+    let step = n_workers as u64;
+
     std::thread::scope(|s| {
-        let stop = Arc::new(AtomicBool::new(false));
-        let result = Arc::new(AtomicU64::new(0));
-        let n_workers = available_parallelism().unwrap().get();
-        let step = n_workers as u64;
-        for i in 0..step {
-            let input = input.clone();
-            let stop = stop.clone();
-            let result = result.clone();
-            s.spawn(move || {
-                let mut salt = i;
-                while !stop.load(Ordering::Relaxed) {
-                    if test_salt(input.clone(), salt, leading_zeros) {
-                        result.store(salt, Ordering::Relaxed);
-                        stop.store(true, Ordering::Relaxed);
+        let workers: Vec<_> = (0..step)
+            .map(|i| {
+                let input = input.clone();
+                let result = result.clone();
+                s.spawn(move || {
+                    let mut salt = i;
+                    while result.load(Ordering::Acquire) > salt {
+                        if test_salt(input.clone(), salt, leading_zeros) {
+                            result.fetch_min(salt, Ordering::AcqRel);
+                        }
+                        salt += step;
                     }
-                    salt += step;
-                }
-            });
-        }
-        while !stop.load(Ordering::Relaxed) {
-            std::hint::spin_loop();
+                })
+            })
+            .collect();
+
+        for worker in workers {
+            worker.join().unwrap();
         }
         result.load(Ordering::Relaxed)
     })
