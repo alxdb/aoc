87a2a653491fa376045df3ee376370e8dff27d83
alxdb <alxdb@pm.me>
2024-12-03 09:44:42 +0000

    2024 day 1 part 1

--- src/solutions/year_2024/day_01.rs
+++ src/solutions/year_2024/day_01.rs
@@ -8,6 +8,10 @@ impl FromStr for Input {
     fn from_str(s: &str) -> Result<Self, Box<dyn Error>> {
         let mut input = Input(vec![], vec![]);
         for line in s.split('\n') {
+            if line.is_empty() {
+                break;
+            }
+
             let mut nums = line.split("   ");
             let num1 = nums.next().ok_or("Lists are not of equal length")?;
             input.0.push(u32::from_str(num1)?);
@@ -23,5 +27,32 @@ pub fn part1(input: &str) -> Result<u32, Box<dyn Error>> {
     input.0.sort_unstable();
     input.1.sort_unstable();
 
-    todo!()
+    let mut total_distance = 0;
+    for item in input.0.into_iter().zip(input.1) {
+        if item.0 > item.1 {
+            total_distance += item.0 - item.1;
+        } else {
+            total_distance += item.1 - item.0;
+        }
+    }
+
+    Ok(total_distance)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::part1;
+    use crate::{fetch_input, AocId};
+    use std::error::Error;
+
+    #[test]
+    fn test_part1() -> Result<(), Box<dyn Error>> {
+        let answer = part1(&fetch_input(AocId {
+            year: 2024,
+            day: 1,
+            part: 1,
+        })?)?;
+        assert_eq!(answer, 2264607);
+        Ok(())
+    }
 }
