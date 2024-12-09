b466f53a26527e1aefab74975ad604675d082f99
alxdb <alxdb@pm.me>
2024-12-09 23:46:18 +0000

    2024 day 7 prep
    Answers included as this problem was already solved.
    
    Also dealing with differing output kinds.

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use aoc_rust::solutions;
 use aoc_rust::{fetch_input, AocId};
 use clap::Parser;
 use std::error::Error;
+use std::fmt::Display;
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -23,67 +24,77 @@ fn main() -> Result<(), Box<dyn Error>> {
     };
 
     let input = fetch_input(aoc_id)?;
-    let answer = match aoc_id {
+    let answer: Box<dyn Display> = match aoc_id {
         AocId {
             year: 2024,
             day: 1,
             part: 1,
-        } => solutions::year_2024::day_01::part1(&input)?,
+        } => Box::new(solutions::year_2024::day_01::part1(&input)?),
         AocId {
             year: 2024,
             day: 1,
             part: 2,
-        } => solutions::year_2024::day_01::part2(&input)?,
+        } => Box::new(solutions::year_2024::day_01::part2(&input)?),
         AocId {
             year: 2024,
             day: 2,
             part: 1,
-        } => solutions::year_2024::day_02::part1(&input)?,
+        } => Box::new(solutions::year_2024::day_02::part1(&input)?),
         AocId {
             year: 2024,
             day: 2,
             part: 2,
-        } => solutions::year_2024::day_02::part2(&input)?,
+        } => Box::new(solutions::year_2024::day_02::part2(&input)?),
         AocId {
             year: 2024,
             day: 3,
             part: 1,
-        } => solutions::year_2024::day_03::part1(&input)?,
+        } => Box::new(solutions::year_2024::day_03::part1(&input)?),
         AocId {
             year: 2024,
             day: 3,
             part: 2,
-        } => solutions::year_2024::day_03::part2(&input)?,
+        } => Box::new(solutions::year_2024::day_03::part2(&input)?),
         AocId {
             year: 2024,
             day: 4,
             part: 1,
-        } => solutions::year_2024::day_04::part1(&input)?,
+        } => Box::new(solutions::year_2024::day_04::part1(&input)?),
         AocId {
             year: 2024,
             day: 4,
             part: 2,
-        } => solutions::year_2024::day_04::part2(&input)?,
+        } => Box::new(solutions::year_2024::day_04::part2(&input)?),
         AocId {
             year: 2024,
             day: 5,
             part: 1,
-        } => solutions::year_2024::day_05::part1(&input)?,
+        } => Box::new(solutions::year_2024::day_05::part1(&input)?),
         AocId {
             year: 2024,
             day: 5,
             part: 2,
-        } => solutions::year_2024::day_05::part2(&input)?,
+        } => Box::new(solutions::year_2024::day_05::part2(&input)?),
         AocId {
             year: 2024,
             day: 6,
             part: 1,
-        } => solutions::year_2024::day_06::part1(&input)?,
+        } => Box::new(solutions::year_2024::day_06::part1(&input)?),
         AocId {
             year: 2024,
             day: 6,
             part: 2,
-        } => solutions::year_2024::day_06::part2(&input)?,
+        } => Box::new(solutions::year_2024::day_06::part2(&input)?),
+        AocId {
+            year: 2024,
+            day: 7,
+            part: 1,
+        } => Box::new(solutions::year_2024::day_07::part1(&input)?),
+        AocId {
+            year: 2024,
+            day: 7,
+            part: 2,
+        } => Box::new(solutions::year_2024::day_07::part2(&input)?),
         _ => todo!(),
     };
     println!("answer={}", answer);
---
+++ src/solutions/year_2024/day_07.rs
@@ -0,0 +1,64 @@
+use std::error::Error;
+
+pub fn part1(input: &str) -> Result<u64, Box<dyn Error>> {
+    todo!()
+}
+
+pub fn part2(input: &str) -> Result<u64, Box<dyn Error>> {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{part1, part2};
+    use crate::{fetch_input, AocId};
+    use std::error::Error;
+
+    const EXAMPLE_INPUT: &str = "190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20";
+
+    #[test]
+    fn test_part1_example() -> Result<(), Box<dyn Error>> {
+        assert_eq!(part1(EXAMPLE_INPUT)?, 3749);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part1() -> Result<(), Box<dyn Error>> {
+        assert_eq!(
+            part1(&fetch_input(AocId {
+                year: 2024,
+                day: 7,
+                part: 1
+            })?)?,
+            932137732557
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn test_part2_example() -> Result<(), Box<dyn Error>> {
+        assert_eq!(part2(EXAMPLE_INPUT)?, 11387);
+        Ok(())
+    }
+
+    #[test]
+    fn test_part2() -> Result<(), Box<dyn Error>> {
+        assert_eq!(
+            part2(&fetch_input(AocId {
+                year: 2024,
+                day: 7,
+                part: 2
+            })?)?,
+            661823605105500
+        );
+        Ok(())
+    }
+}
