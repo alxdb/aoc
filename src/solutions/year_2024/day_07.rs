0e646ba647259fbd77d8bb0d52ba09769f0f81ef
alxdb <alxdb@pm.me>
2024-12-10 00:43:00 +0000

    2024 day 7 parts 1 and 2

--- src/solutions/year_2024/day_07.rs
+++ src/solutions/year_2024/day_07.rs
@@ -1,11 +1,81 @@
 use std::error::Error;
 
+struct Input(Vec<Equation>);
+
+struct Equation {
+    target: u64,
+    numbers: Vec<u64>,
+}
+
+impl TryFrom<&str> for Input {
+    type Error = Box<dyn Error>;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        value
+            .lines()
+            .map(|line| -> Result<_, Self::Error> {
+                let (target, numbers) = line.split_once(": ").ok_or("Invalid input")?;
+                Ok(Equation {
+                    target: target.parse()?,
+                    numbers: numbers
+                        .split(' ')
+                        .map(|n| n.parse::<u64>())
+                        .collect::<Result<Vec<_>, _>>()?,
+                })
+            })
+            .collect::<Result<Vec<_>, _>>()
+            .map(Input)
+    }
+}
+
+fn concat(a: u64, b: u64) -> u64 {
+    if b == 0 {
+        a * 10
+    } else {
+        a * 10u64.pow(b.ilog10() + 1) + b
+    }
+}
+
+impl Equation {
+    fn is_valid(&self, use_concat: bool) -> bool {
+        let mut partials = vec![self.numbers[0]];
+        for number in &self.numbers[1..] {
+            let mut new_partials = Vec::with_capacity(partials.len() * 3);
+            for partial in partials {
+                if partial <= self.target {
+                    new_partials.push(partial + number);
+                    new_partials.push(partial * number);
+                    if use_concat {
+                        new_partials.push(concat(partial, *number));
+                    }
+                }
+            }
+            partials = new_partials;
+        }
+        partials.contains(&self.target)
+    }
+}
+
 pub fn part1(input: &str) -> Result<u64, Box<dyn Error>> {
-    todo!()
+    let input = Input::try_from(input)?;
+    let mut total_calibration_result = 0;
+    for equation in input.0 {
+        if equation.is_valid(false) {
+            total_calibration_result += equation.target;
+        }
+    }
+    Ok(total_calibration_result)
 }
 
 pub fn part2(input: &str) -> Result<u64, Box<dyn Error>> {
-    todo!()
+    let input = Input::try_from(input)?;
+    let mut total_calibration_result = 0;
+    for equation in input.0 {
+        if equation.is_valid(true) {
+            total_calibration_result += equation.target;
+        }
+    }
+    Ok(total_calibration_result)
 }
 
 #[cfg(test)]
